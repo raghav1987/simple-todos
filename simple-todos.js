37414a4582c4806c45e57348fc7c568a0c84a235
Raghav Malik <raghav1987@gmail.com>
2015-03-23 17:56:02 +0530

    finish local app implementation

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,36 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({}, {sort: {text: 0}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() //current time
+      });
+
+      event.target.text.value = "";
+
+      return false;
     }
   });
+
+  Template.task.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {$set: {checked: !this.checked}});
+  },
+  "click .delete": function () {
+    Tasks.remove(this._id);
+  }
+});
 }
 
 if (Meteor.isServer) {
